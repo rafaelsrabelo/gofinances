007a28a13565803acabf24de5fe397377cacfb97
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2021-11-09 19:22:58 -0300

    criando Header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
-import { Container, Title } from './styles'
+import { 
+    Container, 
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon
+} from './styles'
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo 
+                        source={{ uri: 'https://avatars.githubusercontent.com/u/54684348?v=4'}}/>
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Rafael</UserName>
+                        </User>
+                    </UserInfo>
+                    
+                    <Icon name="power" />
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
