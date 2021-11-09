2a184ed2f4c722759399764f4df23d7aa601312f
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2021-11-09 16:46:25 -0300

    estilos globais]

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -4,10 +4,11 @@ export const Container = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
+    /* background: ${({ theme }) => theme.colors.primary} */
 `
 
 export const Title = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.bold};
     font-size: 24px;
-    font-weight: bold;
     color: purple;
 `
